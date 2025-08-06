6662d27d4de1711221712e9631f4ac910d8a1743
Jowitzki77 <jakob.jovan.hrustl@gmail.com>
2025-08-06 13:34:53 +0200

    fixed that container works

--- src/js/Components/Header/Header.js
+++ src/js/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
+import container from "../../../scss/layout/_grid.scss"
 
 import "./_header.scss";
 
 const Header = (props) => {
     return (
-        <div className={props.type}>
-            {props.children}
+        <div className="container">
+            <div className={props.type}>
+                {props.children}
+            </div>
         </div>
+
     )
 }
 export default Header;
