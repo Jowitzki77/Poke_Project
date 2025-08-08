cea20353b0f2c7c7cbb01e03b9ea147266b268ec
Jowitzki77 <jakob.jovan.hrustl@gmail.com>
2025-08-08 19:41:31 +0200

    done some work

--- src/js/Components/MainContent/Maincontent.js
+++ src/js/Components/MainContent/Maincontent.js
@@ -6,9 +6,9 @@ const MainContent = () => {
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-2 article__main">
+                    <div className="col-2 article__main left__article">
                         <h2 className="infoboard__title"> Informacije!</h2>
-                        <h3 className="infoboard__description">Kje</h3>
+                        <h3 className="infoboard__description">Kje ?</h3>
                         <p className="infoboard__text"> Mariborska cesta 2, 3000 Celje </p>
                         <a className="naslov" href="https://www.google.com/maps/place/MC+Celje/@46.231634,15.267626,17z/data=!3m1!4b1!4m6!3m5!1s0x476e2c7f8d9f0a5b:0x7d8c6e2f8c8f8c8f!8m2!3d46.231634!4d15.267626!16s%2Fg%2F11c1l5j9qv?entry=ttu" target="_blank" rel="noopener noreferrer">Google Maps Lokacija : 📍 </a>
                         <h3 className="infoboard__description">Kdaj?</h3>
