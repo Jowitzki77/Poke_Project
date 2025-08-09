29c76782415eb069239aae110beec9872a61addd
Jowitzki77 <jakob.jovan.hrustl@gmail.com>
2025-08-09 23:09:59 +0200

    fixed picture position and finished footer

--- src/js/Components/Footer/Footer.js
+++ src/js/Components/Footer/Footer.js
@@ -5,15 +5,16 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__contact">
+                <p className="footer__info__header">KONTAKTIRAJTE NAS:</p>
                 <p className="footer__info">
-                    📞 <a href="tel:+38651369988">+386 51 369 988</a>
+                    <a className="footer__text" href="tel:+38651369988">+386 51 369 988</a>
                 </p>
                 <p className="footer__info">
-                    ✉️ <a href="mailto:example@email.com">example@email.com</a>
+                    <a className="footer__text" href="mailto:example@email.com">example@email.com</a>
                 </p>
             </div>
             <div className="footer__rights">
-                <p>© {new Date().getFullYear()} All rights reserved.</p>
+                <p className="footer__rights__text">© {new Date().getFullYear()} Poke Party Cele, All rights reserved.</p>
             </div>
         </footer>
     );
