55b45bed705a7f6abbffbf64308d17070ff0a738
Jowitzki77 <jakob.jovan.hrustl@gmail.com>
2025-08-11 16:35:05 +0200

    done ish home page

--- src/js/Components/MainContent/Maincontent.js
+++ src/js/Components/MainContent/Maincontent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./_maincontent.scss"
+import "../../../scss/general/_buttons.scss"
 
 const MainContent = () => {
     return (
@@ -15,8 +16,23 @@ const MainContent = () => {
                         <p className="infoboard__text">6.9.2025 od 10:00 do 17:00</p>
 
                     </div>
-                    <div className="col-2 article__main">
-                        Right
+                    <div className="col-2 article__main right__article">
+                        <div className="article__event">
+                            <h2 className="article__turnir__title">Poke Trznica</h2>
+                            <p className="article__turnir__text">Pridružite se nam in raziskujte pri stevilnih razstavljalcih</p>
+                            <p className="article__turnir__text">Menjaj, prodaj, kupi ali pa kar tako, namenjeno je vsem!</p>
+                            <p className="article__turnir__text">Zelis imeti svojo mizo? Pisi nam, saj je stevilo miz omejeno!</p>
+                            <button className="btn btn__turnir__info">Klikni za več informacij</button>
+                        </div>
+                        <div className="article__event">
+                            <div className="article__event">
+                                <h2 className="article__turnir__title">Turnir</h2>
+                                <p className="article__turnir__text">Pridružite se nam na našem prvem turnirju v Pokemon kartah!</p>
+                                <p className="article__turnir__text">Bogate nagrade za najboljše tri igralce!</p>
+                                <p className="article__turnir__text">Prijavnine <span className="poke__span">NI</span></p>
+                                <button className="btn btn__turnir__info">Klikni za več informacij</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
